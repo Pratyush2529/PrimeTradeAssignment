58dfd24f5cbb0a5f6ff9fe6a7dcb0a26943fb55e
Pratyush Sharma <pratyushsharma25feb@gmail.com>
2026-01-02 03:57:09 +0530

    fix for cookie setting in prod

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -36,8 +36,8 @@ export const register = async (req, res, next) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000,
         });
 
@@ -74,8 +74,8 @@ export const login = async (req, res, next) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000,
         });
 
